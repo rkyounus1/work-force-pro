0f942ff0627efecdee0ca74a7090aae1ec7c3074
rkyounus1 <rkyounus1@example.com>
2025-04-29 14:18:56 +0530

    Initial commit with setup files

---
+++ src/test/java/com/futurestack/workforcepro/WorkforceproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurestack.workforcepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkforceproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
